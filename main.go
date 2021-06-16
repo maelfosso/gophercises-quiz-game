fee8ad58f3c0229e2acfb29da5444156056bba25
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-16 06:38:30 +0100

    loaded quizzes file

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func loadQuizzesFile() ([][]string, error) {
+	file, err := os.Open("./problems.csv")
+	if err != nil {
+		return nil, fmt.Errorf("error encountered when opening quizzes file, %v", err)
+	}
+
+	csvReader := csv.NewReader(file)
+
+	return csvReader.ReadAll()
+}
+
 func main() {
-	// Read the CSV file
+	// Load the CSV file
+	dataFile, err := loadQuizzesFile()
+	if err != nil {
+		log.Fatalf("error occured when loading quizzes file, %v", err)
+	}
 
 	// Parse its content
 

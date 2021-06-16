3227a318731dc5b860442e654351c65d963628ee
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-16 06:23:41 +0100

    steps of my main

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// Read the CSV file
+
+	// Parse its content
+
+	// Map into struct the read content
+
+	// Loop through all the quizzes for the user to solve them
+}
